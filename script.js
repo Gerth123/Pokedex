ebbcc0778a1c81e6cfc6a71214f78b9650bd4bb2
Gerth123 <robingerth21@gmail.com>
2024-04-15 23:44:28 +0200

    added the OpenAbout - OpenMoves Functions, to switch between the Stats.

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ let typeColors = {
     'Ice': 'rgb(151, 217, 240)',
 };
 
+let pokemonInfoBottom = {
+    'About': true,
+    'BaseStats': false,
+    'Evolution': false,
+    'Moves': false,
+};
+
+let pokemonInfoBottomForLoop = ['pokemonInfoBottomAbout', 'pokemonInfoBottomBaseStats', 'pokemonInfoBottomEvolution', 'pokemonInfoBottomMoves']
+
+// nidoran-f im großen PokemonContainer
+
 let url = 'https://pokeapi.co/api/v2/pokemon/';
 
 let currentPokemon;
@@ -34,6 +45,8 @@ let actualPokemonImgUrl = [];
 let actualTypes = [];
 
 let pokemonsIndexBig = [];
+
+let correctedWeight = [];
 
 async function loadPokemon(actualUrl) {
     let response = await fetch(actualUrl);
@@ -97,6 +110,9 @@ async function includeTypes(actualUrl) {
 function setSecondActualType() {
     if (actualTypes.length == 2) {
         return actualTypes[1];
+    } else {
+        let nothing = '';
+        return nothing;
     }
 }
 
@@ -130,19 +146,22 @@ function colorOfType2(pokemonsIndex) {
 
 function capitalizeFirstLetter(actualPokemon) {
     if (actualPokemon == 'mr-mime') {
-        let mrMimeCapitalized = 'Mr. Mime';
+        let actualPokemonCapitalized = 'Mr. Mime';
         actualPokemonUsed.splice(0, 1);
-        actualPokemonUsed.push(mrMimeCapitalized);
+        actualPokemonUsed.push(actualPokemonCapitalized);
+        return actualPokemonCapitalized;
     } else { 
     if (actualPokemon == 'nidoran-f'){
-        let nidoranFCapitalized = 'Nidoran Female';
+        let actualPokemonCapitalized = 'Nidoran Female';
         actualPokemonUsed.splice(0, 1);
-        actualPokemonUsed.push(nidoranFCapitalized);
+        actualPokemonUsed.push(actualPokemonCapitalized);
+        return actualPokemonCapitalized;
     }  else {
     if (actualPokemon == 'nidoran-m'){
-        let nidoranMCapitalized = 'Nidoran Male';
+        let actualPokemonCapitalized = 'Nidoran Male';
         actualPokemonUsed.splice(0, 1);
-        actualPokemonUsed.push(nidoranMCapitalized);
+        actualPokemonUsed.push(actualPokemonCapitalized);
+        return actualPokemonCapitalized;
     }
     else {
         let firstLetter = actualPokemon.charAt(0);
@@ -151,6 +170,7 @@ function capitalizeFirstLetter(actualPokemon) {
         let actualPokemonCapitalized = firstLetterCapitalized + remainingLetters;
         actualPokemonUsed.splice(0, 1);
         actualPokemonUsed.push(actualPokemonCapitalized);
+        return actualPokemonCapitalized;
     } } }
 }
 
@@ -161,26 +181,25 @@ function renderPokemonInfo(pokemonsIndex) {
 
 
 async function openPokemonInfo (pokemonsIndex, actualUrl, pokemonNumber) {
+    // let audio = new Audio(`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/'${pokemonsIndex}.ogg`);
+    // audio.play();
     let body = document.getElementById('body');
     body.innerHTML += '';
     await body.classList.remove('d-none');
 
     let actualPokemon = pokemons[pokemonsIndex];
-    let firstLetter = actualPokemon.charAt(0);
-    let remainingLetters = actualPokemon.substring(1);
-    let firstLetterCapitalized = firstLetter.toUpperCase();
-    let actualPokemonCapitalized = firstLetterCapitalized + remainingLetters;
     let response = await fetch(actualUrl);
     let responseAsJson = await response.json();
     await includeTypes(actualUrl);
     await capitalizeFirstLetter(actualPokemon);
+    let correctWeight = responseAsJson['weight'];
     
     body.innerHTML = /*html*/`
     <div class="pokemonInfoContainerAndBorder" id="pokemonInfoContainerAndBorder">
         <div class="pokemonInfoContainer"  onclick="dontClose(event)" id="pokemonInfoContainer${pokemonsIndex}">
             <div class="pokemonInfoHeadlineContainer"> 
                 <div class="pokemonInfoHeadlineAndNumberContainer">
-                <h2 class="pokemonInfoHeadline">${actualPokemonCapitalized}</h2><span class="numberOfPokemonInfo">#${pokemonNumber}</span>
+                <h2 class="pokemonInfoHeadline">${capitalizeFirstLetter(actualPokemon)}</h2><span class="numberOfPokemonInfo">#${pokemonNumber}</span>
                 </div>
                 <span class="pokemonInfoTypeContainer"><div class="pokemonInfoTypeBackgroundColor">${actualTypes[0]}</div><div class="pokemonInfoTypeBackgroundColorTwo" id="typeBackgroundColorTwo${pokemonsIndex}">${setSecondActualType()}</div></span>
                 </div>
@@ -189,24 +208,24 @@ async function openPokemonInfo (pokemonsIndex, actualUrl, pokemonNumber) {
             </div>
             <div class="pokemonInfoBottomContainer" id="pokemonInfoBottomContainer">
                 <div class="pokemonInfoBottomHeadlineContainer">
-                    <span class="pokemonInfoBottomAbout" id="pokemonInfoBottomAbout${pokemonsIndex}">About</span>
-                    <span class="pokemonInfoBottomBaseStats">Base Stats</span>
-                    <span class="pokemonInfoBottomEvolution">Evolution</span>
-                    <span class="pokemonInfoBottomMoves">Moves</span>
+                    <span class="pokemonInfoBottomAbout pokemonInfoBottomOpened" id="pokemonInfoBottomAbout${pokemonsIndex}" onclick="openAbout('${pokemonsIndex}', '${responseAsJson}')">About</span>
+                    <span class="pokemonInfoBottomBaseStats" id="pokemonInfoBottomBaseStats${pokemonsIndex}" onclick="openBaseStats(${pokemonsIndex})">Base Stats</span>
+                    <span class="pokemonInfoBottomEvolution" id="pokemonInfoBottomEvolution${pokemonsIndex}" onclick="openEvolution(${pokemonsIndex})">Evolution</span>
+                    <span class="pokemonInfoBottomMoves" id="pokemonInfoBottomMoves${pokemonsIndex}" onclick="openMoves(${pokemonsIndex})">Moves</span>
                 </div>
-                <div class="wholeIntegratedElementsAndTypesInfoBottomContainer">
-                    <div class="integratedElementsAndTypesInfoBottomContainer">
+                <div class="wholeIntegratedElementsAndTypesInfoBottomContainer" id="wholeIntegratedElementsAndTypesInfoBottomContainer">
+                    <div class="integratedElementsAndTypesInfoBottomContainer" id="integratedElementsAndTypesInfoBottomContainer">
                         <div class="typesForIntegratedElementsBottomContainer">
-                            <span>Species</span>
+                            <span>Species:</span>
                             <span>Height:</span>
                             <span>Weight:</span>
                             <span>Abilities:</span>
                         </div>
                         <div class="integratedElementsInfoBottomContainer">
-                            <span>Whatever</span>
-                            <span>${responseAsJson['height']} Inches</span>
-                            <span>${responseAsJson['weight']} Kilogramm</span>
-                            <span>${responseAsJson['abilities']['0']['ability']['name']}</span>
+                            <span>${actualTypes[0]}${checkComma()} ${setSecondActualType()}</span>
+                            <span>${await correctDates(responseAsJson['height'])} Metres</span>
+                            <span>${await correctDates(correctWeight)} Kilogramm</span>
+                            <span>${await capitalizeFirstLetterUniversal(responseAsJson['abilities']['0']['ability']['name'])}</span>
                         </div>
                     </div>
                     <div class="separationLine" id="separationLine${pokemonsIndex}"></div>
@@ -215,11 +234,11 @@ async function openPokemonInfo (pokemonsIndex, actualUrl, pokemonNumber) {
                         <div class="secondIntegratedElementsAndTypesInfoBottom">    
                             <div class="typesForIntegratedElementsBottomContainer">
                                 <span>Gender:</span>
-                                <span>Egg</span>
-                                <span>Eggcycle</span>
+                                <span>Egg Group:</span>
+                                <span>Egg Cycle:</span>
                             </div>
                             <div class="integratedElementsInfoBottomContainer"> 
-                                <span>Gender:</span>
+                                <span>Male</span>
                                 <span>Egg</span>
                                 <span>Eggcycle</span>
                             </div>
@@ -230,11 +249,37 @@ async function openPokemonInfo (pokemonsIndex, actualUrl, pokemonNumber) {
         </div> 
     </div>
     `;
+    
     await checkSecondActualType(pokemonsIndex);
     await colorOfType2(pokemonsIndex);
     await colorOfLine(pokemonsIndex);
     actualTypes.splice(0, 2);
     pokemonsIndexBig.push(pokemonsIndex);
+}
+
+function correctDates(value) {
+    let correctWeightString = value.toString(); // Konvertiere die Zahl in eine Zeichenkette
+    let lengthOfWeight = correctWeightString.length - 1;
+    let lastNumber = correctWeightString.charAt(lengthOfWeight);
+    let correctWeightLength = correctWeightString.length;
+    if(correctWeightLength === 1){
+        let remainingNumbers = 0;
+        let newValue = remainingNumbers + ',' + lastNumber;
+        return newValue;
+    } else{
+        let remainingNumbers = correctWeightString.slice(0, lengthOfWeight);
+        let newValue = remainingNumbers + ',' + lastNumber;
+        return newValue;
+    }
+    
+}
+
+function capitalizeFirstLetterUniversal(value){
+    let firstLetter = value.charAt(0);
+    let remainingLetters = value.substring(1);
+    let firstLetterCapitalized = firstLetter.toUpperCase();
+    let actualValue = firstLetterCapitalized + remainingLetters;
+    return actualValue;
 }
 
 async function closePokemonInfo() {
@@ -249,12 +294,93 @@ function dontClose(event) {
 function colorOfLine(pokemonsIndex) {
     let actualSeparationLine = 'separationLine' + pokemonsIndex;
     let separationLine = document.getElementById(actualSeparationLine);
-    let actualPokemonInfoBottomAbout = 'pokemonInfoBottomAbout' + pokemonsIndex;
-    let pokemonInfoBottomAbout = document.getElementById(actualPokemonInfoBottomAbout);
+    // let actualPokemonInfoBottomAbout = 'pokemonInfoBottomAbout' + pokemonsIndex;
+    // let pokemonInfoBottomAbout = document.getElementById(actualPokemonInfoBottomAbout);
 
     let backgroundColor = typeColors[actualTypes[0]];
     if (backgroundColor) {
         separationLine.style.backgroundColor = backgroundColor;
-        pokemonInfoBottomAbout.style.textDecorationColor = backgroundColor;
+        // pokemonInfoBottomAbout.style.textDecorationColor = backgroundColor;
+    }
+}
+
+function checkComma() {
+    if(actualTypes.length !== 1){
+        let actualComma = ',';
+        return actualComma;
+    } else {
+        let actualComma = '';
+        return actualComma;
+    }
+}
+
+function openAbout(pokemonsIndex, responseAsJson) {
+    let container = document.getElementById('wholeIntegratedElementsAndTypesInfoBottomContainer');
+    container.classList.remove('d-none');
+    let actualPokemonInfoBottomAbout = 'pokemonInfoBottomAbout' + pokemonsIndex;
+    let pokemonInfoBottomAbout = document.getElementById(actualPokemonInfoBottomAbout);
+    setPokemonInfoBottomFalse();
+    pokemonInfoBottom[0] = true;
+    removeBottomOpened(pokemonsIndex);
+    pokemonInfoBottomAbout.classList.add('pokemonInfoBottomOpened');
+    let actualTypeForBackground = responseAsJson['types'][0]['type']['name'];
+    let actualTypeForBackgroundCapitalized = capitalizeFirstLetterUniversal(actualTypeForBackground);
+    let backgroundColor = typeColors[actualTypes[actualTypeForBackgroundCapitalized]];
+    pokemonInfoBottomAbout.style.textDecorationColor = backgroundColor;
+}
+
+function openBaseStats(pokemonsIndex) {
+    let container = document.getElementById('wholeIntegratedElementsAndTypesInfoBottomContainer');
+    container.classList.add('d-none');
+    let actualPokemonInfoBottomBaseStats = 'pokemonInfoBottomBaseStats' + pokemonsIndex;
+    let pokemonInfoBottomBaseStats = document.getElementById(actualPokemonInfoBottomBaseStats);
+    let backgroundColor = typeColors[actualTypes[0]];
+    pokemonInfoBottomBaseStats.style.textDecorationColor = backgroundColor;
+    setPokemonInfoBottomFalse();
+    pokemonInfoBottom[1] = true;
+    removeBottomOpened(pokemonsIndex);
+    pokemonInfoBottomBaseStats.classList.add('pokemonInfoBottomOpened');
+}
+
+function openEvolution(pokemonsIndex) {
+    let container = document.getElementById('wholeIntegratedElementsAndTypesInfoBottomContainer');
+    container.classList.add('d-none');
+    let actualPokemonInfoBottomEvolution = 'pokemonInfoBottomEvolution' + pokemonsIndex;
+    let pokemonInfoBottomEvolution = document.getElementById(actualPokemonInfoBottomEvolution);
+    let backgroundColor = typeColors[actualTypes[0]];
+    pokemonInfoBottomEvolution.style.textDecorationColor = backgroundColor;
+    setPokemonInfoBottomFalse();
+    pokemonInfoBottom[2] = true;
+    removeBottomOpened(pokemonsIndex);
+    pokemonInfoBottomEvolution.classList.add('pokemonInfoBottomOpened');
+}
+
+function openMoves(pokemonsIndex) {
+    let container = document.getElementById('wholeIntegratedElementsAndTypesInfoBottomContainer');
+    container.classList.add('d-none');
+    let actualPokemonInfoBottomMoves = 'pokemonInfoBottomMoves' + pokemonsIndex;
+    let pokemonInfoBottomMoves = document.getElementById(actualPokemonInfoBottomMoves);
+    let backgroundColor = typeColors[actualTypes[0]];
+    pokemonInfoBottomMoves.style.textDecorationColor = backgroundColor;
+    setPokemonInfoBottomFalse();
+    pokemonInfoBottom[3] = true;
+    removeBottomOpened(pokemonsIndex);
+    pokemonInfoBottomMoves.classList.add('pokemonInfoBottomOpened');
+}
+
+function setPokemonInfoBottomFalse() {
+    for (let pokemonInfoFalseIndex = 0; pokemonInfoFalseIndex < pokemonInfoBottom.length; pokemonInfoFalseIndex++) {
+        pokemonInfoBottom[pokemonInfoFalseIndex] = false;
+    }
+}
+
+function removeBottomOpened(pokemonsIndex) {
+    for (let pokemonInfoBottomIndex = 0; pokemonInfoBottomIndex < pokemonInfoBottomForLoop.length; pokemonInfoBottomIndex++) {
+        let actualPokemonInfoBottomOpened = pokemonInfoBottomForLoop[pokemonInfoBottomIndex];
+        if (actualPokemonInfoBottomOpened !== true) {
+            let pokemonInfoBottomOpenedForId = pokemonInfoBottomForLoop[pokemonInfoBottomIndex] + pokemonsIndex;
+            let getElementByIdPokemonInfoBottom = document.getElementById(pokemonInfoBottomOpenedForId);
+            getElementByIdPokemonInfoBottom.classList.remove('pokemonInfoBottomOpened');
+        }
     }
 }
